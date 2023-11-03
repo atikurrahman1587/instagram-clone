cc68e10ec7a04de790228b0e5e75c8330f100879
Atikur <atikurrahman1587@gmailcom>
2023-11-03 21:06:19 +0600

    add the caption and input box of the post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,6 +3,7 @@ import { BsThreeDots } from "react-icons/bs";
 import { AiOutlineHeart } from "react-icons/ai";
 import { BsChatDots } from "react-icons/bs";
 import { BiBookmark } from "react-icons/bi";
+import { HiOutlineEmojiHappy } from "react-icons/hi";
 export default function Post({id, username, userImg, img, caption}) {
   return (
     <div className="bg-white my-7 border rounded-md">
@@ -22,6 +23,14 @@ export default function Post({id, username, userImg, img, caption}) {
           </div>
           <BiBookmark className="btn"/>
         </div>
+        {/* Post Comment */}
+        <p className="p-5 truncate"><span className="font-bold mr-2">{username}</span>{caption}</p>
+        {/* Post input box */}
+        <form action="" className="flex items-center p-4">
+          <HiOutlineEmojiHappy className="h-7"/>
+          <input className="border-none flex-1 focus:ring-0" type="text" placeholder="Enter your comment..."/>
+          <button className="text-blue-400 font-bold">Post</button>
+        </form>
     </div>
   )
 }
