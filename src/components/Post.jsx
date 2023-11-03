ab1eb29f1cc2b59b64ae36cd1b16967478f85e14
Atikur <atikurrahman1587@gmailcom>
2023-11-03 20:13:43 +0600

    create the header and image section of the post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
-
+import { BsThreeDots } from "react-icons/bs";
 export default function Post({id, username, userImg, img, caption}) {
   return (
-    <div>{username}</div>
+    <div className="bg-white my-7 border rounded-md">
+        {/* Post Header */}
+        <div className="flex items-center p-5">
+          <img className="h-12 rounded-full object-cover border p-1" src={userImg} alt={username}/>
+          <p className="font-bold flex-1">{username}</p>
+          <BsThreeDots className="h-5"/>
+        </div>
+        {/* Post image */}
+        <img className="object-cover w-full" src={img} alt=""/>
+    </div>
   )
 }
